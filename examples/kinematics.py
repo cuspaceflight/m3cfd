12d819da7e40921ccdab17355202c4b1b8a2f92c
Rich Wareham <rjw57@cam.ac.uk>
2016-03-08 16:29:27 +0000

    kinematics.py: fix indentation
    Python convention is to use four spaces for indent and to avoid trailing
    whitespace. Re-indent the file to follow these conventions.

--- examples/kinematics.py
+++ examples/kinematics.py
@@ -15,7 +15,7 @@ posits = np.zeros([times.shape[0],3],float) # x,y,z - 3 DoF postion
 angles = np.zeros([times.shape[0],3],float) # theta, psi, roll  - 3 DoF rotation
 # Theta is defined as the angle down from the vertical (z) axis - the angle between z and z'
 # Psi is defined as the rotation around the vertical (z) axis
-# Roll is defined as the rotation around the z' axis 
+# Roll is defined as the rotation around the z' axis
 
 # To begin with model rocket as a cylinder
 Initial_Mass = 100
@@ -28,63 +28,63 @@ vel = np.zeros(3,float) # start at the origin with zero velocity
 rot = np.zeros(3,float)
 
 for ii, t in enumerate(times):
-	if ii > 0:
-		dt = t - times[ii-1]
+    if ii > 0:
+        dt = t - times[ii-1]
 
-		if t <= 50: 
-			Thrust = 2000.0
-			print Thrust
-		else: Thrust = 0.0
+        if t <= 50:
+            Thrust = 2000.0
+            print Thrust
+        else: Thrust = 0.0
 
-		angles[ii,:] = angles[ii-1,:]
-		posits[ii,:] = posits[ii-1,:]
+        angles[ii,:] = angles[ii-1,:]
+        posits[ii,:] = posits[ii-1,:]
 
-		## Mass integration ------------------------------------------
-		
-		mdot = 0.1 # kg / s 	
+        ## Mass integration ------------------------------------------
 
-		mass0 = mass*1
-		mass -= mdot
-		timestepmass = 0.5*(mass + mass0)
-	
-		# Moments of inertia from geometry + engine mass
-		Ixx = (timestepmass/12.0)*( 3*Radius**2 + Height**2 )
-		Iyy = (timestepmass/12.0)*( 3*Radius**2 + Height**2 )
-		Izz = timestepmass*Radius**2 / 2.0
-		MoI = np.array([Ixx,Iyy,Izz])
+        mdot = 0.1 # kg / s
 
-		# Forces and moments from CFD solution - in body coordinates
-		moments = np.array([0.0, 0.0, 0.0])  # Nm = kg m^2 / s^2
-		rotdotbody = moments/MoI
+        mass0 = mass*1
+        mass -= mdot
+        timestepmass = 0.5*(mass + mass0)
 
-		theta = angles[ii,0]
-		rotmat = 	np.array([	[np.cos(theta), 0, np.sin(theta)],
-								[0, 1, 0],
-								[-1*np.sin(theta), 0, np.cos(theta)] ])
-		
-		rotdotglobal = np.dot(rotmat,rotdotbody) 
+        # Moments of inertia from geometry + engine mass
+        Ixx = (timestepmass/12.0)*( 3*Radius**2 + Height**2 )
+        Iyy = (timestepmass/12.0)*( 3*Radius**2 + Height**2 )
+        Izz = timestepmass*Radius**2 / 2.0
+        MoI = np.array([Ixx,Iyy,Izz])
 
-		## Moments integration ------------------------------------------
-		
-		# Rotation integration from angular acceleration
-		rot0 = rot*1
-		rot += rotdotglobal*dt 
-		angles[ii,:] += 0.5*(rot0 + rot)*dt	
+        # Forces and moments from CFD solution - in body coordinates
+        moments = np.array([0.0, 0.0, 0.0])  # Nm = kg m^2 / s^2
+        rotdotbody = moments/MoI
 
-		## Forces integration ------------------------------------------
-		forces = np.array([2.0, 0.0, Thrust])  # Nm = kg m / s^2
-		#pdb.set_trace()		
-		vdotbody = forces/timestepmass
-	
-		vdotglobal = np.dot(rotmat,vdotbody)
-		vdotglobal[-1] += -gravityacc
+        theta = angles[ii,0]
+        rotmat =    np.array([  [np.cos(theta), 0, np.sin(theta)],
+                                [0, 1, 0],
+                                [-1*np.sin(theta), 0, np.cos(theta)] ])
 
-		# Convert forces from body to global
+        rotdotglobal = np.dot(rotmat,rotdotbody)
 
-		# Velocity integration from acceleration
-		vel0 = vel*1 # *1 to avoid copying variable reference
-		vel += vdotglobal*dt
-		posits[ii,:] += 0.5*(vel + vel0)*dt
+        ## Moments integration ------------------------------------------
+
+        # Rotation integration from angular acceleration
+        rot0 = rot*1
+        rot += rotdotglobal*dt
+        angles[ii,:] += 0.5*(rot0 + rot)*dt
+
+        ## Forces integration ------------------------------------------
+        forces = np.array([2.0, 0.0, Thrust])  # Nm = kg m / s^2
+        #pdb.set_trace()
+        vdotbody = forces/timestepmass
+
+        vdotglobal = np.dot(rotmat,vdotbody)
+        vdotglobal[-1] += -gravityacc
+
+        # Convert forces from body to global
+
+        # Velocity integration from acceleration
+        vel0 = vel*1 # *1 to avoid copying variable reference
+        vel += vdotglobal*dt
+        posits[ii,:] += 0.5*(vel + vel0)*dt
 
 plt.figure()
 plt.scatter(times[:],posits[:,0])
@@ -101,7 +101,7 @@ plt.ylabel('z')
 plt.show()
 
 
-	
+
 
 
 
